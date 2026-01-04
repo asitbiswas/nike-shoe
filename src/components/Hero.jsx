246a9492b8bf05803af9682234261a1df9c2c1ba
Asit Biswas <asitbiswas@Asits-MacBook-Air.local>
2026-01-04 12:52:27 +0530

    chaged the images path as they were not showing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,13 +15,13 @@ function HeroSection(){
           <div className="shop">
             <p>Also Available On</p>
             <div className="brand-icons">
-              <img src="/images/flipkart.png" alt="flipkart-logo" />
-              <img src="/images/amazon.png" alt="amazon-logo" />
+              <img src="./images/flipkart.png" alt="flipkart-logo" />
+              <img src="...../images/amazon.png" alt="amazon-logo" />
             </div>
           </div>
         </div>
         <div className="hero-image">
-          <img src="/images/shoe_image.png" alt="shoe-image" />
+          <img src="./images/shoe_image.png" alt="shoe-image" />
         </div>
       </main>
     );
